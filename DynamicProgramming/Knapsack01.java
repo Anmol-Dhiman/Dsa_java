ffea50a1300de6f4a7e214cf71b0b703b4bf8eaf
Anmol-Dhiman <anmoldhiman7111@gmail.com>
2022-07-23 12:05:30 +0530

    knapsack memoization

--- DynamicProgramming/Knapsack01.java
+++ DynamicProgramming/Knapsack01.java
@@ -2,15 +2,29 @@ package DynamicProgramming;
 
 import java.util.Scanner;
 
+import javax.swing.text.html.FormView;
+
 public class Knapsack01 {
-    
+
+    private static int[][] t = new int[7][7];
+
     public static void main(String[] args) {
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                t[i][j] = -1;
+            }
+        }
+
         Scanner in = new Scanner(System.in);
 
         int[] val = new int[5];
         int[] wgt = new int[5];
 
         System.out.print("Enter the weight of knapsack : ");
+
+        // here i take the maximum value of w as 5
+
         int w = in.nextInt();
 
         for (int i = 0; i < wgt.length; i++) {
@@ -21,7 +35,7 @@ public class Knapsack01 {
         }
         in.close();
 
-        System.out.print("maximum profit : " + knapsack_profit(w, wgt, val, 5));
+        System.out.print("maximum profit : " + knapsack_profit_memoization(w, wgt, val, 5));
 
     }
 
@@ -29,12 +43,27 @@ public class Knapsack01 {
         if (n == 0 || w == 0)
             return 0;
 
-        if(wgt[n-1]<=w){
-            return  max(val[n-1]+ knapsack_profit(w - wgt[n-1], wgt, val, n-1),
-            knapsack_profit(w , wgt, val, n-1));
+        if (wgt[n - 1] <= w) {
+            return max(val[n - 1] + knapsack_profit(w - wgt[n - 1], wgt, val, n - 1),
+                    knapsack_profit(w, wgt, val, n - 1));
+        } else {
+            return knapsack_profit(w, wgt, val, n - 1);
         }
-        else{
-          return  knapsack_profit(w , wgt, val, n-1);
+
+    }
+
+    private static int knapsack_profit_memoization(int w, int[] wgt, int[] val, int n) {
+        if (n == 0 || w == 0)
+            return 0;
+
+        if (t[n][w] != -1)
+            return t[n][w];
+
+        if (wgt[n - 1] <= w) {
+            return t[n][w] = max(val[n - 1] + knapsack_profit(w - wgt[n - 1], wgt, val, n - 1),
+                    knapsack_profit(w, wgt, val, n - 1));
+        } else {
+            return t[n][w] = knapsack_profit(w, wgt, val, n - 1);
         }
 
     }
@@ -44,4 +73,5 @@ public class Knapsack01 {
             return i;
         return j;
     }
+
 }
