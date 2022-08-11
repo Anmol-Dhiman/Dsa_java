1a5434a8cd59b023f27a7b6770d1d80c7ce33a38
Anmol-Dhiman <anmoldhiman7111@gmail.com>
2022-08-11 19:23:58 +0530

    count with diff

--- DynamicProgramming/CountSubsetGivenDiff.java
+++ DynamicProgramming/CountSubsetGivenDiff.java
@@ -26,8 +26,31 @@ public class CountSubsetGivenDiff {
 
         // subset one sum : -
         int firstSubsetSum = (diff + sum) / 2;
-        int secondSubsetSum = sum - firstSubsetSum;
-        
 
+        int[][] dp = new int[n + 1][firstSubsetSum + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < firstSubsetSum + 1; j++) {
+                if (i == 0)
+                    dp[i][j] = 0;
+                if (j == 0)
+                    dp[i][j] = 1;
+            }
+        }
+        System.out.print("The number of subset that exists for the given difference " + diff + " is "
+                + countSubsetWithDiff(n, firstSubsetSum, a, dp));
+
+    }
+
+    private static int countSubsetWithDiff(int n, int sum, int[] a, int[][] dp) {
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (a[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j - a[i - 1]] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][sum];
     }
 }
