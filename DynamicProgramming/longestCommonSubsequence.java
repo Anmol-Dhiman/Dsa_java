361d8866b487a12bb8be7b7a41b55f0744d99434
Anmol-Dhiman <anmoldhiman7111@gmail.com>
2022-08-13 11:57:13 +0530

    memoization lcs

--- DynamicProgramming/longestCommonSubsequence.java
+++ DynamicProgramming/longestCommonSubsequence.java
@@ -12,21 +12,44 @@ public class longestCommonSubsequence {
         System.out.print("Enter the second String : ");
         y = sc.nextLine();
         sc.close();
+        int[][] t = new int[x.length() + 1][y.length() + 1];
+        for (int i = 0; i <= x.length(); i++) {
+            for (int j = 0; j <= y.length(); j++) {
+                if (i == 0 || j == 0) {
+                    t[i][j] = 0;
+                }
+            }
+        }
 
-        System.out.print("the lenght of longest common subsequence is : " + lcs(x, y, x.length(), y.length()));
+        System.out
+                .println("the lenght of longest common subsequence is : " + lcsRecursive(x, y, x.length(), y.length()));
+        System.out.println(
+                "the lenght of longest common subsequence is : " + lcsMemoization(x, y, x.length(), y.length(), t));
 
     }
 
-    private static int lcs(String x, String y, int n, int m) {
+    private static int lcsRecursive(String x, String y, int n, int m) {
 
         if (n == 0 || m == 0)
             return 0;
 
         if (x.charAt(n - 1) == y.charAt(m - 1)) {
-            return 1 + lcs(x, y, n - 1, m - 1);
+            return 1 + lcsRecursive(x, y, n - 1, m - 1);
         } else {
-            return Math.max(lcs(x, y, n - 1, m), lcs(x, y, n, m - 1));
+            return Math.max(lcsRecursive(x, y, n - 1, m), lcsRecursive(x, y, n, m - 1));
         }
-
     }
+
+    private static int lcsMemoization(String x, String y, int n, int m, int[][] t) {
+        if (n == 0 || m == 0)
+            return 0;
+
+        if (x.charAt(n - 1) == y.charAt(m - 1)) {
+            t[n][m] = 1 + lcsRecursive(x, y, n - 1, m - 1);
+        } else {
+            t[n][m] = Math.max(lcsRecursive(x, y, n - 1, m), lcsRecursive(x, y, n, m - 1));
+        }
+        return t[n][m];
+    }
+
 }
