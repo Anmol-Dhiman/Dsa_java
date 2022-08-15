fec191023f3f2c29f633a184745c39017883feef
Anmol-Dhiman <anmoldhiman7111@gmail.com>
2022-08-15 21:45:16 +0530

    minimum insertion deletion

--- DynamicProgramming/MinInsertionDeletion.java
+++ DynamicProgramming/MinInsertionDeletion.java
@@ -2,7 +2,7 @@ package DynamicProgramming;
 
 import java.util.Scanner;
 
-public class ShortestCommonSuperSequence {
+public class MinInsertionDeletion {
     public static void main(String[] args) {
         String x, y;
         Scanner sc = new Scanner(System.in);
@@ -19,28 +19,30 @@ public class ShortestCommonSuperSequence {
                     dp[i][j] = 0;
             }
         }
+        int lcs = lcsTopDown(x, y, x.length(), y.length(), dp);
 
-        System.out.print("The lenght of shortest common super sequence is : "
-                + shortestSubsequence(x, y, x.length(), y.length(), dp));
+        System.out.println("the number of insertion " + (y.length() - lcs));
+        System.out.println("the number of deletion " + (x.length() - lcs));
 
     }
 
-    private static int shortestSubsequence(String x, String y, int n, int m, int[][] dp) {
-        String temp = x + y;
-        return temp.length() - lcsTopDown(x, y, n, m, dp);
-    }
+    private static int lcsTopDown(String x, String y, int n, int m, int[][] dp) {
 
-    private static int lcsTopDown(String x, String y, int n, int m, int[][] t) {
+        int result = 0;
+
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
                 if (x.charAt(i - 1) == y.charAt(j - 1)) {
-                    t[i][j] = 1 + t[i - 1][j - 1];
+
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    result = Math.max(result, dp[i][j]);
+
                 } else {
-                    t[i][j] = Math.max(t[i - 1][j], t[i][j - 1]);
+                    dp[i][j] = 0;
                 }
             }
         }
-        return t[n][m];
+        return result;
     }
+
 }
- 
