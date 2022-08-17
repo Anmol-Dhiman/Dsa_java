5c2cdb08488a306cd7e8b979f40ba262b6bef888
Anmol-Dhiman <anmoldhiman7111@gmail.com>
2022-08-17 11:20:12 +0530

    bottomUp MCM

--- DynamicProgramming/MatrixMultiplicationCost.java
+++ DynamicProgramming/MatrixMultiplicationCost.java
@@ -15,8 +15,16 @@ public class MatrixMultiplicationCost {
             arr[i] = sc.nextInt();
         }
         sc.close();
+        int[][] t = new int[n + 1][n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                t[i][j] = -1;
+            }
+        }
 
-        System.out.print("the minimum cost to multiply the matrix is : " + minCost(arr, 1, n - 1));
+        System.out.println("the minimum cost to multiply the matrix is : " + minCost(arr, 1, n - 1));
+        System.out
+                .println("Bottom up the minimum cost to multiply the matrix is : " + minCostBottomUp(arr, 1, n - 1, t));
 
     }
 
@@ -32,5 +40,24 @@ public class MatrixMultiplicationCost {
             cost = Math.min(cost, temp);
         }
         return cost;
+    }
+
+    private static int minCostBottomUp(int[] a, int i, int j, int[][] t) {
+
+        if (i == j)
+            return 0;
+
+        if (t[i][j] != -1)
+            return t[i][j];
+        int cost = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+
+            // a[i - 1] * a[k] * a[j] this step is to get the cost for the temperary solved
+            // matrix
+
+            int temp = minCost(a, i, k) + minCost(a, k + 1, j) + a[i - 1] * a[k] * a[j];
+            cost = Math.min(cost, temp);
+        }
+        return t[i][j] = cost;
     }
 }
