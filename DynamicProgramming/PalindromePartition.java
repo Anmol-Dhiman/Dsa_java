fac7777b8bce945d24c06293ce835e4f7c8367ca
Anmol-Dhiman <anmoldhiman7111@gmail.com>
2022-08-18 18:26:56 +0530

    palindrome partition memoization

--- DynamicProgramming/PalindromePartition.java
+++ DynamicProgramming/PalindromePartition.java
@@ -1,6 +1,5 @@
 package DynamicProgramming;
 
-import java.security.cert.X509CRLEntry;
 import java.util.Scanner;
 
 public class PalindromePartition {
@@ -9,9 +8,19 @@ public class PalindromePartition {
         System.out.print("Enter the string : ");
         Scanner sc = new Scanner(System.in);
         x = sc.nextLine();
+        sc.close();
+        int[][] t = new int[x.length() + 1][x.length() + 1];
+        for (int i = 0; i <= x.length(); i++) {
+            for (int j = 0; j <= x.length(); j++) {
+                t[i][j] = -1;
+            }
+        }
 
         System.out.println(
                 "Minimum partitions to make every string as palindrome : " + palindromePartition(x, 0, x.length() - 1));
+        System.out.println(
+                "Memoization Minimum partitions to make every string as palindrome : "
+                        + palindromePartitionMemoization(x, 0, x.length() - 1, t));
 
     }
 
@@ -29,13 +38,29 @@ public class PalindromePartition {
     }
 
     private static boolean isPalindrome(String x, int i, int j) {
-        while(i < j)
-        {
-          if(x.charAt(i) != x.charAt(j))
-            return false; 
-          i++;
-          j--;
+        while (i < j) {
+            if (x.charAt(i) != x.charAt(j))
+                return false;
+            i++;
+            j--;
         }
         return true;
+    }
+
+    private static int palindromePartitionMemoization(String x, int i, int j, int[][] t) {
+
+        if (i == j)
+            return 0;
+        if (isPalindrome(x, i, j))
+            return t[i][j] = 0;
+
+        if (t[i][j] != -1)
+            return t[i][j];
+        int ans = Integer.MAX_VALUE, temp;
+        for (int k = i; k <= j - 1; k++) {
+            temp = palindromePartition(x, i, k) + palindromePartition(x, k + 1, j) + 1;
+            ans = Math.min(ans, temp);
+        }
+        return t[i][j] = ans;
     }
 }
